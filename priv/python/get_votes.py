81b3f4f7ad275e75bb2a1b0088f30b7caf64e31c
Henoc Díaz <henocdz@gmail.com>
2016-06-21 05:11:53 -0500

    Refacto insert_vote, get_votes and get_votes_per_candidate functions

--- priv/python/get_votes.py
+++ priv/python/get_votes.py
@@ -1,15 +1,33 @@
 import rethinkdb as r
-
+​
 from utils import get_bigchain
-
+​
 def get_votes_for_candidate(bigchain, candidate_id):
-    cursor = r.table('bigchain').concat_map(lambda doc: doc["block"]["transactions"].filter(lambda transaction: r.expr([400, 600]).contains(transaction["transaction"]["data"]["payload"]["candidate_id"]))).run(bigchain.conn)
-    print(cursor)
-
+    """Calculates total of votes for given candidate"""
+    return r.table('bigchain').concat_map(
+        lambda doc: doc["block"]["transactions"].filter(
+            lambda transaction: transaction["transaction"]["data"]["payload"]["candidate_id"] == candidate_id
+        ).map(
+            lambda transaction: transaction["transaction"]["data"]["payload"])
+    ).count().run(bigchain.conn)
+​
+​
 def get_total_votes(bigchain):
-    data = r.table('bigchain').concat_map(lambda doc: doc["block"]["transactions"].group(lambda transaction: transaction["transaction"]["data"]["payload"]["candidate_id"]).count()).run(bigchain.conn)
-    print(data)
-
+    """Calculates total of votes per candidate"""
+    data = r.table('bigchain').group(lambda doc: doc["block"]["transactions"]["transaction"]["data"]["payload"]["candidate_id"]).count().run(bigchain.conn)
+​
+    # Let's clean this ...
+    # Because (I don't know why but...) this query returns something likes this:
+    # {(600,): 6, (): 11, (400,): 2}
+    # We need to feed our clients with something smoother
+    results = {}
+    for k, v in data.items():
+        if k and hasattr(k, '__iter__'):
+            results[k[0]] = v
+​
+    return results
+​
 if __name__ == '__main__':
     connection = get_bigchain()
-    get_votes_for_candidate(connection, 400)
+    print(get_votes_for_candidate(connection, 500))
+    print(get_total_votes(connection))
--- priv/python/insert_vote.py
+++ priv/python/insert_vote.py
@@ -1,7 +1,7 @@
 from utils import get_bigchain, get_node_keys
 import time
-
-
+​
+​
 def insert_vote(candidate_id=400):
     """Insert a vote into the Chain with Bigchain DB
     Args:
@@ -17,22 +17,21 @@ def insert_vote(candidate_id=400):
         'candidate_id': candidate_id,
         'region_id': 0
     }
-
+​
     # Create transaction uses the operation `CREATE` and has no inputs
     transaction = connection.create_transaction(connection.me, keys.public_key, None, 'CREATE', payload=payload)
     # All transactions need to be signed by the user creating the transaction
     transaction_signed = connection.sign_transaction(transaction, connection.me_private)
-
+​
     # Write the transaction to the bigchain.
     # The transaction will be stored in a backlog where it will be validated,
     # included in a block, and written to the bigchain
     connection.write_transaction(transaction_signed)
-    print(transaction_signed['id'])
+    print('Procesando...')
     time.sleep(15)
     # Retrieve a transaction from the bigchain
     transaction_retrieved = connection.get_transaction(transaction_signed.get('id'))
-    print(transaction_retrieved)
-
-
+​
+​
 if __name__ == '__main__':
-    insert_vote(600)
+    insert_vote(500)
