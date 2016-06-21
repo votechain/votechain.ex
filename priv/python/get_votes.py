17dab0ab144ab6d5c0edfa373609ec8c998b3d1b
Henoc Díaz <henocdz@gmail.com>
2016-06-21 03:23:13 -0500

    Add insert_db and get_votes_for_candidate functions

---
+++ priv/python/get_votes.py
@@ -0,0 +1,12 @@
+import rethinkdb as r
+
+from .utils import get_bigchain
+
+def get_votes_for_candidate(bigchain, candidate_id):
+    cursor = r.table('bigchain').concat_map(lambda doc: doc["block"]["transactions"].filter(lambda transaction: transaction["transaction"]["data"]["payload"]["candidate_id"].match(400))).run(bigchain.conn)
+    print(cursor)
+
+
+if __name__ == '__main__':
+    connection = get_bigchain()
+    get_votes_for_candidate(connection, 400)
