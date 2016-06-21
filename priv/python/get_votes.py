c59bdd12bac551c06bf233394d31c0614052c783
Henoc Díaz <henocdz@gmail.com>
2016-06-21 05:14:07 -0500

    Remove invalid chars

--- priv/python/get_votes.py
+++ priv/python/get_votes.py
@@ -1,7 +1,7 @@
 import rethinkdb as r
-​
+
 from utils import get_bigchain
-​
+
 def get_votes_for_candidate(bigchain, candidate_id):
     """Calculates total of votes for given candidate"""
     return r.table('bigchain').concat_map(
@@ -10,12 +10,12 @@ def get_votes_for_candidate(bigchain, candidate_id):
         ).map(
             lambda transaction: transaction["transaction"]["data"]["payload"])
     ).count().run(bigchain.conn)
-​
-​
+
+
 def get_total_votes(bigchain):
     """Calculates total of votes per candidate"""
     data = r.table('bigchain').group(lambda doc: doc["block"]["transactions"]["transaction"]["data"]["payload"]["candidate_id"]).count().run(bigchain.conn)
-​
+
     # Let's clean this ...
     # Because (I don't know why but...) this query returns something likes this:
     # {(600,): 6, (): 11, (400,): 2}
@@ -24,9 +24,9 @@ def get_total_votes(bigchain):
     for k, v in data.items():
         if k and hasattr(k, '__iter__'):
             results[k[0]] = v
-​
+
     return results
-​
+
 if __name__ == '__main__':
     connection = get_bigchain()
     print(get_votes_for_candidate(connection, 500))
